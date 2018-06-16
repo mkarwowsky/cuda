e39be2870d016e5173b21cd52226ffca9c6d5022
mkarwowsky <mkarwowsky@gmail.com>
2018-06-16 18:32:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,15 +18,17 @@ int main(int argc, char** argv) {
     // Read the arguments
     const char* input_file = argv[1+j];
     const char* output_file = argv[2+j];
-
+    printf("Czytanie argumentów: %s %s \n", argv[1+j], argv[2+j]);
     std::vector<unsigned char> in_image;
     unsigned int width, height;
 
     // Load the data
+    printf("Ładowanie danych \n");
     unsigned error = lodepng::decode(in_image, width, height, input_file);
     if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-
+    
     // Prepare the data
+    printf("Przygotowanie danych \n");
     unsigned char* input_image = new unsigned char[(in_image.size()*3)/4];
     unsigned char* output_image = new unsigned char[(in_image.size()*3)/4];
     int where = 0;
@@ -39,9 +41,11 @@ int main(int argc, char** argv) {
     }
 
     // Run the filter on it
+    printf("Filter uruchom \n");
     filter(input_image, output_image, width, height); 
 
     // Prepare data for output
+    printf("Dane na output \n");
     std::vector<unsigned char> out_image;
     for(int i = 0; i < in_image.size(); ++i) {
         out_image.push_back(output_image[i]);
@@ -51,6 +55,7 @@ int main(int argc, char** argv) {
     }
     
     // Output the data
+    printf("Output data \n");
     error = lodepng::encode(output_file, out_image, width, height);
     
     //if there's an error, display it
