0779778a25e927c7d36097746020b2acb3a1770f
mkarwowsky <mkarwowsky@gmail.com>
2018-06-17 14:55:52 +0200

    Add files via upload

--- kernels.cu
+++ kernels.cu
@@ -38,8 +38,6 @@ void blur(unsigned char* input_image, unsigned char* output_image, int width, in
 
 void filter (unsigned char* input_image, unsigned char* output_image, int width, int height, unsigned char* dev_input, unsigned char* dev_output ) {
 
-//   unsigned char* dev_input;
-//   unsigned char* dev_output;
     getError(cudaMalloc( (void**) &dev_input, width*height*3*sizeof(unsigned char)));
     getError(cudaMemcpy( dev_input, input_image, width*height*3*sizeof(unsigned char), cudaMemcpyHostToDevice ));
  
@@ -50,10 +48,6 @@ void filter (unsigned char* input_image, unsigned char* output_image, int width,
 
     blur<<<gridDims, blockDims>>>(dev_input, dev_output, width, height); 
 
-    getError(cudaMemcpy(output_image, dev_output, width*height*3*sizeof(unsigned char), cudaMemcpyDeviceToHost ));
-
-    getError(cudaFree(dev_input));
-    getError(cudaFree(dev_output));
 
 }
 
